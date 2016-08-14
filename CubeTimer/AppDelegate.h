221f01dc6b7bd7dad50f39def17ce2dde53d0b71
westonmizumoto <westonm1@stanford.edu>
2016-08-13 17:50:00 -0700

    Initial Commit

---
+++ CubeTimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CubeTimer
+//
+//  Created by Weston Mizumoto on 8/13/16.
+//  Copyright © 2016 Weston Mizumoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CubeTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CubeTimer
+//
+//  Created by Weston Mizumoto on 8/13/16.
+//  Copyright © 2016 Weston Mizumoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
